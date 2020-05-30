a2680596023a11fb30900615de611938ccdecf8c
Doğukan Köse <dogukankse4@gmail.com>
2020-05-31 01:31:30 +0300

    lightinig settings

--- Assets/_Scripts/SunController.cs
+++ Assets/_Scripts/SunController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,29 +6,34 @@ namespace _Scripts
 {
     public class SunController : MonoBehaviour
     {
-        [Header("Sun")] [SerializeField] private Light sun;
+        [Header("Sun")] [SerializeField] private Light _sun;
 
         [Tooltip("Simulate 24H in seconds")] [SerializeField]
-        private float secondsInDay = 120f;
+        private float _secondsInDay = 120f;
 
         [Tooltip("midnight => 0\nsunrise => 0.25\nnoon => 0.5\nsunset => 0.75")] [Range(0f, 1f)] [SerializeField]
         public float currentTime;
 
-        private float timeMultiplier = 1f;
+        private const float _timeMultiplier = 1f;
 
-        private float sunInitialIntensity;
-        [SerializeField] private bool realtime;
+        [SerializeField] private float _sunInitialIntensity;
+        [SerializeField] private bool _realtime;
 
         private List<Light> _roadLamps;
 
+        private void Awake()
+        {
+            _sun = GetComponent<Light>();
+        }
+
         private void Start()
         {
-            sunInitialIntensity = sun.intensity;
+            _sunInitialIntensity = 1f;
         }
 
         public void FindRoadLamps()
         {
-            GameObject[] lamps = GameObject.FindGameObjectsWithTag("RoadLamp");
+            var lamps = GameObject.FindGameObjectsWithTag("RoadLamp");
             _roadLamps = new List<Light>();
 
             foreach (var lamp in lamps)
@@ -37,8 +43,8 @@ namespace _Scripts
         private void Update()
         {
             UpdateSun();
-            if (!realtime) return;
-            currentTime += (Time.deltaTime / secondsInDay) / timeMultiplier;
+            if (!_realtime) return;
+            currentTime += (Time.deltaTime / _secondsInDay) / _timeMultiplier;
 
             if (currentTime >= 1) currentTime = 0;
         }
@@ -48,18 +54,18 @@ namespace _Scripts
         /// </summary>
         private void UpdateSun()
         {
-            sun.transform.localRotation = Quaternion.Euler((currentTime * 360f) - 90f, 140f, 0f);
+            _sun.transform.localRotation = Quaternion.Euler((currentTime * 360f) - 90f, 140f, 0f);
 
             float intensityMultiplier = 1.5f;
-            if (currentTime <= 0.23f || currentTime >= 0.75f)
+            if (currentTime <= 0.25f || currentTime >= 0.75f)
                 intensityMultiplier = 0;
-            else if (currentTime <= 0.25f)
-                intensityMultiplier = Mathf.Clamp01((currentTime - 0.23f) * 50f);
-            else if (currentTime >= 0.73f)
-                intensityMultiplier = Mathf.Clamp01((currentTime - 0.73f) * 50f);
+            else if (currentTime >= 0.25f)
+                intensityMultiplier = Mathf.Clamp01((currentTime - 0.25f) * 50f);
+            else if (currentTime <= 0.75f)
+                intensityMultiplier = Mathf.Clamp01((currentTime - 0.75f) * 50f);
 
 
-            sun.intensity = sunInitialIntensity * intensityMultiplier;
+            _sun.intensity = _sunInitialIntensity * intensityMultiplier;
 
             UpdateRoadLamps(1 - intensityMultiplier);
         }
